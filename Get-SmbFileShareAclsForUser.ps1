03c19dd0fbef0416b4b6046dc6a6d9b6f6f233c8
Alex Harmon <aharmon@zebeth.org>
2019-06-28 18:36:14 -0700

    Add files via upload

---
+++ Get-SmbFileShareAclsForUser.ps1
@@ -0,0 +1,11 @@
+﻿$username = "user"
+$outfile = "C:\Users\user\Desktop\out.txt"
+$path = Get-ChildItem "\\smb\share\" -Recurse -Directory
+Foreach( $file in $path ) {
+	$f = Get-Acl $file.FullName
+	if( $f.Owner -CMatch $username ) {
+		Write-Host( "{0}"-f $file.FullName | Out-File `
+			-Encoding "UTF8" `
+			-FilePath $outfile -Append)
+	}
+}
